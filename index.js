a38be8c477e1722ffb81a48a120f5236e1b93686
Jaydeep Shah <jaydeepshah300@gmail.com>
2022-09-13 00:33:30 +0530

    Created asyncthunk action in redux toolkit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
   require("./features/icecream/icecreamSlice").icecreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial State", store.getState());
 
@@ -9,13 +10,15 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
 
-unsubscribe();
+store.dispatch(fetchUsers());
+
+// unsubscribe();
